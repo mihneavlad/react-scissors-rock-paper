57374187ce8afa92bc8af2bf154c4e8ffd27d91d
mihneavlad <mihnel@yahoo.com>
2018-10-30 16:39:42 +0100

    First working version of the game.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component} from 'react';
 import './css/main.css';
 
 // stateless component
@@ -6,13 +6,12 @@ import './css/main.css';
 const PlayerCard = ({color, symbol}) => {
 
 	const style = {
-		backgroundColor: color
+		backgroundColor: color,
+		backgroundImage: "url(./img/" + symbol + ".png)"
 	}
 
 	return (
-		<div style={style} className="player-card">
-			{symbol}
-		</div>
+		<div style={style} className="player-card"></div>
 	)
 }
 
@@ -24,28 +23,56 @@ class App extends Component {
 		this.state = {};
 	}
 
+	decideWinner = () => {
+		const {player1, player2} = this.state;
+		if (player1 === player2) {
+			return "It's a draw!"
+		}
+		if(
+			(player1 === "rock" && player2 ==="scissors") ||
+			(player1 === "scissors" && player2 ==="paper") ||
+			(player1 === "paper" && player2 ==="rock")
+			) {
+			return "Player 1 is the winner!"
+		}
+		else {
+			return "Player 2 is the winner!"
+		}
+	}
+
 	playGame = () => {
-		const symbol = Math.floor(Math.random() * 3);
-		console.log(this.symbols[symbol]);
-		this.setState({
-			player1: this.symbols[Math.floor(Math.random() * 3)],
-			player2: this.symbols[Math.floor(Math.random() * 3)]
-		})
+
+		let counter = 0
+
+		let shuffle = setInterval(() => {
+			counter++
+			this.setState({
+				player1: this.symbols[Math.floor(Math.random() * 3)],
+				player2: this.symbols[Math.floor(Math.random() * 3)],
+				winner: ""
+			})
+
+			if(counter > 15) {
+				clearInterval(shuffle);
+				this.setState({winner: this.decideWinner})
+			}
+		}, 100)
 	}
 
 	render() {
 		return (
-			<div className="App">
-				<PlayerCard
-					color="red"
-					symbol={this.state.player1}
-				/>
-				<PlayerCard
-					color="blue"
-					symbol={this.state.player2}
-				/>
-				<button onClick={this.playGame}>Play Game</button>
-			</div>
+				<div className="App">
+					<PlayerCard
+						color="red"
+						symbol={this.state.player1}
+					/>
+					<PlayerCard
+						color="blue"
+						symbol={this.state.player2}
+					/>
+					<p>{this.state.winner}</p>
+					<button onClick={this.playGame}>Play Game</button>
+				</div>
 		);
 	}
 }
