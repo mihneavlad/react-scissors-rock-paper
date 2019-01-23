2e928183dc4424d7d0f964e15e1797064ed52a0c
mihneavlad <mihnel@yahoo.com>
2019-01-23 17:22:39 +0100

    Fixed error attempt

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,21 +1,17 @@
-import React, {Fragment} from 'react';
-import { Link } from 'react-router-dom';
-import hero from '../hero.jpg';
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+// import hero from '../hero.jpg';
 
-
-const Home = (props) => (
-
-    <Fragment>
-      <div className="intro-page d-flex">
-        <div className="m-auto">
-          <Link to="/personview" className="btn btn-main">
-            Feel like a Rock–Paper–Scissors challenge?
-          </Link>
-
-        </div>
+const Home = props => (
+  <Fragment>
+    <div className="intro-page d-flex">
+      <div className="m-auto">
+        <Link to="/personview" className="btn btn-main">
+          Feel like a Rock–Paper–Scissors challenge?
+        </Link>
       </div>
-    </Fragment>
-
-)
+    </div>
+  </Fragment>
+);
 
 export default Home;
