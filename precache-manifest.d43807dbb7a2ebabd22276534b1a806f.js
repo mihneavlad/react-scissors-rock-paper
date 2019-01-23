a60c9ab5f46f5d697fbaab665d92a395df5255f9
mihneavlad <mihnel@yahoo.com>
2019-01-23 17:54:54 +0100

    Updates

---
+++ precache-manifest.d43807dbb7a2ebabd22276534b1a806f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d343b1067cf1719261a112b838c588e3",
+    "url": "/react-scissors-rock-paper/static/media/hero.d343b106.jpg"
+  },
+  {
+    "revision": "550213a45aa768fd2180",
+    "url": "/react-scissors-rock-paper/static/js/runtime~main.550213a4.js"
+  },
+  {
+    "revision": "824f24454c2764c19660",
+    "url": "/react-scissors-rock-paper/static/js/main.824f2445.chunk.js"
+  },
+  {
+    "revision": "f01c7e9d57b1dce50bea",
+    "url": "/react-scissors-rock-paper/static/js/1.f01c7e9d.chunk.js"
+  },
+  {
+    "revision": "824f24454c2764c19660",
+    "url": "/react-scissors-rock-paper/static/css/main.4591e161.chunk.css"
+  },
+  {
+    "revision": "64d81c960fc40110b88fd7b4897fae61",
+    "url": "/react-scissors-rock-paper/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/react-scissors-rock-paper/precache-manifest.d43807dbb7a2ebabd22276534b1a806f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-scissors-rock-paper/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
